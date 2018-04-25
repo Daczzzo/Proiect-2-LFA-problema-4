0116c8cf9020d651999db813dc9d584fe30f5d4a
Daczzzo <daczo.robert@yahoo.com>
2018-04-25 22:50:53 +0300

    Proiect 2 LFA problema 4

---
+++ Proiect/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+using namespace std;
+char stare_init, stari_fin[10];
+char cuvant[100];
+int v[100], pozitie_cur;
+ifstream f("date.in");
+struct tranzitie {
+    char actual;
+    char car;
+    char urm;
+} tranz[20];
+
+/**
+*
+*@param stare_act reprezinta nodul in care este programul in acest moment
+*@param cuvant reprezinta un string care se modifica de la o apelare recursiva la alta
+*eliminand prima litera a cuvantului
+*@param nr_muchii este numarul total de muchii, fiecare muchie fiind reprezentata de o litera
+*@param nr_fin este numarul de stari finale ale automatului
+*/
+
+void gramatica_regulata(char stare_act, char cuvant[100], int nr_muchii, int nr_fin) {
+    int i, OK = 0;
+    char copie_cuvant[100];
+    if (cuvant[0] == NULL) {
+        for (i = 0; i < nr_fin; i++)
+            if (stare_act == stari_fin[i]) {
+                v[pozitie_cur] = 1;
+                pozitie_cur++;
+                OK = 1;
+            }
+        if (OK == 0) {
+            v[pozitie_cur] = 0;
+            pozitie_cur++;
+        }
+    }
+    for (i = 0; i < nr_muchii; i++)
+        if (tranz[i].actual == stare_act && cuvant[0] == tranz[i].car) {
+            strcpy(copie_cuvant, cuvant + 1);
+            gramatica_regulata(tranz[i].urm, copie_cuvant, nr_muchii, nr_fin);
+        }
+
+}
+
+int main() {
+    int nr_muchii, nr_fin = 0;
+    int i, OK = 0;
+    f >> nr_muchii;
+    for (i = 0; i < nr_muchii; i++) {
+        f >> tranz[i].actual;
+        if (i == 0)
+            stare_init = tranz[0].actual;
+        f >> tranz[i].car;
+        f >> tranz[i].urm;
+    }
+    for (i = 0; i < nr_muchii; i++)
+        if (tranz[i].car == '#') {
+            stari_fin[nr_fin] = tranz[i].actual;
+            nr_fin++;
+        }
+
+    cout << "Introduceti cuvantul:" << endl;
+    cin >> cuvant;
+    cout << endl;
+    gramatica_regulata(stare_init, cuvant, nr_muchii, nr_fin);
+    for (i = 0; i < pozitie_cur; i++)
+        if (v[i] == 1 && OK == 0) {
+            OK = 1;
+            cout << "cuvantul apartine" << endl;
+        }
+    if (OK == 0)
+        cout << "cuvantul nu apartine";
+    return 0;
+}
